f0ee8fa67adecf471e72487702e140da30320aa6
lilfortunaa <bogdanchikzaichik@gmail.com>
2025-08-22 12:20:14 +0200

    note-detail-fix

--- types/note.ts
+++ types/note.ts
@@ -3,6 +3,7 @@ export interface Note {
   title: string;
   content: string;
   tag: NoteTag;
+  createdAt: string;
 }
 
 export type NoteTag = 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
